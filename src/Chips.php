4fcbc8964a89db4d5ef18bb970bf9eebabe6f96d
Ryota062204 <jelcovennekens0402@gmail.com>
2024-11-13 20:26:53 +0100

    chips functionalities

--- src/Chips.php
+++ src/Chips.php
@@ -11,58 +11,97 @@ class Chips
  
     private string $color;
     private int $value;
-    private ?int $quantity = null;
+    private int $quantity ;
+    private int $lostChips ;
+    private int $wonChips ;
 
-    public function __construct($color, $value, $quantity){
-        $this->colors = $color;
+
+    public function __construct($color, $value, $quantity, $lostChips, $wonchips){
+        $this->color = $color;
         $this->value = $value;
         $this->quantity = $quantity;
+        $this->lostChips = $lostChips;
+        $this->wonChips = $wonChips;
+
 
 
     }
 
 
-    public function getColor()
+    public function getColor(): string
     {
         return $this->color;
     }
     
-    public function getvalue()
+    public function getvalue(): int
     {
         return $this->value;
     }
     
-    public function getQuantity()
+    public function getLostChips(): int
+    {
+        return $this->lostChips;
+    }
+
+   
+    
+    public function setLostChips(): int
+    {
+        $this->lostChips = $lostChips;
+    }
+
+    public function getQuantity(): int
+    {
+        return $this->quantity;
+    }
+    public function setQuantity(): int
     {
         return $this->quantity;
     }
 
    
-    
-    public function setQuantity()
+
+    public function getwonChips(): int
     {
-        $this->quantity = $quantity;
+        return $this->wonChips;
     }
 
-    public function setColor()
+   
+    
+    public function setwonChips(): int
+    {
+        $this->wonChips = $wonChips;
+    }
+
+    public function setColor(): string
     {
         $this->color = $color;
     }
     
-    public function setvalue()
+    public function setvalue(): int
     {
         $this->value = $value;
     }
    
-    public function totalValue(){
+    public function totalValue()
+    {
 
         return $this->value * $this->quantity;
     }
 
-    public function totalQuantity(){
+    public function totalQuantity()
+    {
 
         return $this->qauntity;
     }
+
+    public function subtract(){
+       return $quantity = $quantity - $lostChips;
+    }
+
+    public function addChips(){
+        return $quantity = $quantity + $wonChips;
+     }
     
     
 
